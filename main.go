0000976c18cfd98d303e70c43069a62363fc2504
M4giic <66871586+M4giic@users.noreply.github.com>
2025-03-23 18:48:34 +0100

    add Repl

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Println("Jelo")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is Jedreks programming langugae!\n", user.Username)
+	fmt.Printf("Rob ta co chce ta\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
