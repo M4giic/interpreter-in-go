79a4d8e8e72fc84d40a406ddd3cf9efe58eeb1c2
M4giic <66871586+M4giic@users.noreply.github.com>
2025-11-09 16:32:37 +0100

    add string and string concatination

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -72,6 +72,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return args[0]
 		}
 		return applyFunction(function, args)
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 	}
 
 	return nil
@@ -121,6 +123,8 @@ func evalInfixExpression(operator string, left object.Object, right object.Objec
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	case operator == "==":
 		return nativeBoolToBooleanObject(left == right)
 	case operator == "!=":
@@ -155,6 +159,18 @@ func evalIntegerInfixExpression(operator string, left object.Object, right objec
 	case "!=":
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+}
+
+func evalStringInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+
+	if operator == "+" {
+		return &object.String{Value: leftVal + rightVal}
+	} else {
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
 }
